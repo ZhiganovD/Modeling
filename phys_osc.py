c76d1f14a8db95449603cbf7bf53f80d7f56b119
ZhiganovD <zhiganov.dv@phystech.edu>
2022-12-12 17:54:29 +0300

    Add RK4 and RK4 drawing

--- phys_osc.py
+++ phys_osc.py
@@ -42,6 +42,7 @@ def acccompar(t, a_e, a_h, diff):
 
 my_data_e = np.loadtxt("data0_1.txt")
 my_data_h = np.loadtxt("data1_1.txt")
+my_data_RK4 = np.loadtxt("data2_1.txt")
 #print(my_data)
 
 n = int(my_data_e[0][0])
@@ -50,21 +51,29 @@ x1 = np.linspace(0.0, 1000.0, 100000)
 y1 = np.sin(omega * x1 + 0)
 #y1 = np.sin(x1)
 
-x2 = []
-y2 = []
-y2_2 = []
+x_e = []
+y_e = []
+y_e_2 = []
 for i in range(1, n + 1):
-    x2.append(my_data_e[i][0])
-    y2.append(my_data_e[i][1])
-    y2_2.append(pow(my_data_e[i][1], 2))
+    x_e.append(my_data_e[i][0])
+    y_e.append(my_data_e[i][1])
+    y_e_2.append(pow(my_data_e[i][1], 2))
     
-x3 = []
-y3 = []
-y3_2 = []
+x_h = []
+y_h = []
+y_h_2 = []
 for i in range(1, n + 1):
-    x3.append(my_data_h[i][0])
-    y3.append(my_data_h[i][1])
-    y3_2.append(pow(my_data_h[i][1], 2))
+    x_h.append(my_data_h[i][0])
+    y_h.append(my_data_h[i][1])
+    y_h_2.append(pow(my_data_h[i][1], 2))
+    
+x_RK = []
+y_RK = []
+y_RK_2 = []
+for i in range(1, n + 1):
+    x_RK.append(my_data_h[i][0])
+    y_RK.append(my_data_h[i][1])
+    y_RK_2.append(pow(my_data_h[i][1], 2))
     
 t = np.linspace(0, len(my_data_e)*0.01, len(my_data_e)-1)
 
@@ -72,11 +81,14 @@ a_e = []
 a_h = []
 diff = []
 for i in range(0, 100000):
-    a_e.append(acc(t, x2, i))
-    a_h.append(acc(t, x3, i))
+    a_e.append(acc(t, x_e, i))
+    a_h.append(acc(t, x_h, i))
     diff.append(abs(a_e[i] - a_h[i]))
 
-modelcompar(t, x1, y1, x2, y2, x3, y3, y2_2, y3_2)
+#modelcompar(t, x1, y1, x_e, y_e, x_h, y_h, y_e_2, y_h_2)
+
+
+modelcompar(t, x1, y1, x_RK, y_RK, x_h, y_h, y_RK_2, y_h_2)
 
 #acccompar(t, a_e, a_h, diff)
 
--- phys_osc_euler.cpp
+++ phys_osc_euler.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 #include <array>
+#include <chrono>
 #include "json.hpp"
 using json = nlohmann::json;
 
@@ -22,7 +23,12 @@ public:
         std::copy_n(s.begin(), s.size(), state.begin());
     }
 
-    MyState eul(double omega) const
+    MyState func(double omega) const
+    {
+        return MyState {std::array<MP,2> {state[1], (-1) * pow(omega, 2) * sin(state[0])}};
+    }
+
+    MyState func_dt(double omega, double t) const
     {
         return MyState {std::array<MP,2> {state[1], (-1) * pow(omega, 2) * sin(state[0])}};
     }
@@ -61,43 +67,106 @@ public:
     static A make_step_eul(A& state,  float dt, std::ofstream &myfile_e, double omega)
     {
         myfile_e << state[0] << ' ' << state[1] << '\n'; 
-        auto tmp = state + state.eul(omega)*dt;
+        auto tmp = state + state.func(omega)*dt;
         state =  tmp;
+        return state;
+    }
+
+    static A make_step_hoin(A& state, float dt, std::ofstream &myfile_h, double omega)
+    {
+        myfile_h << state[0] << ' ' << state[1] << '\n';
+        auto tmp = state + state.func(omega) * dt;
+        state = state + (state.func(omega) + tmp.func(omega)) * dt / 2.0;
+        return state;
+    }
+
+    static A make_step_RK4(A& state, float dt, std::ofstream &myfile_RK4, double omega, double t)
+    {
+        myfile_RK4 << state[0] << ' ' << state[1] << '\n';
+        auto k1 = state.func(omega);
+        auto y0 = state + k1 * (dt / 2);
+        auto k2 = y0.func(omega);
+        auto y1 = state + k2 * (dt / 2);
+        auto k3 = y1.func(omega);
+        auto y2 = state + k3 * dt;
+        auto k4 = y2.func(omega);
+        state = state + (k1 + k2 * 2 + k3 * 2 + k4) * (dt / 6);
         return state;
     }
 };
 
 int main()
 {
+    std::chrono::steady_clock sc;
     std::ifstream argv("new_input.json");
     json data = json::parse(argv);
     std::ofstream myfile_e;
     std::ofstream myfile_h;
-    std::ofstream myfile_acc;
+    std::ofstream myfile_RK4;
+
     myfile_e.open ("data0_1.txt");
     myfile_h.open ("data1_1.txt");
-    myfile_acc.open ("data2_1.txt");
+    myfile_RK4.open ("data2_1.txt");
+
     myfile_e << data["n"] << ' ' << data["omega"] << '\n';
+    myfile_h << data["n"] << ' ' << data["omega"] << '\n';
+    myfile_RK4 << data["n"] << ' ' << data["omega"] << '\n';
+
     MyState<double> A{std::array<double,2>{data["x0"], data["y0"]}};
     MyState<double> B{std::array<double,2>{data["x0"], data["y0"]}};
+    MyState<double> C{std::array<double,2>{data["x0"], data["y0"]}};
+
     auto k = data["xk"];
     const double & my_value = data["xk"];
     double h = (static_cast<double>(data["xk"]) - static_cast<double>(data["x0"])) / static_cast<double>(data["n"]);
+    double t = 0;
+    /*for (int i = 0; i < data["n"]; ++i)
+    {
+        A = Euler<MyState<double>>::make_step_eul(A, 0.01, myfile_e, data["omega"]);
+        B = Euler<MyState<double>>::make_step_hoin(B, 0.01, myfile_h, data["omega"]);
+        C = Euler<MyState<double>>::make_step_RK4(C, 0.01, myfile_RK4, data["omega"], t);
+        t += 0.01;
+    }*/
+
+    auto start_eu = sc.now();
     for (int i = 0; i < data["n"]; ++i)
     {
         A = Euler<MyState<double>>::make_step_eul(A, 0.01, myfile_e, data["omega"]);
     }
+    auto end_eu = sc.now();
 
-    auto step = Euler<MyState<double>>::make_step_eul(A, 0.01, myfile_e, data["omega"]);
+    auto start_h = sc.now();
+    for (int i = 0; i < data["n"]; ++i)
+    {
+        B = Euler<MyState<double>>::make_step_hoin(B, 0.01, myfile_h, data["omega"]);
+    }
+    auto end_h = sc.now();
 
-    std::cout << "hello" << std::endl;
-    MyState<double> X{std::array<double,2>{1.00, 2.00}};
+    auto start_RK = sc.now();
+    for (int i = 0; i < data["n"]; ++i)
+    {
+        C = Euler<MyState<double>>::make_step_RK4(C, 0.01, myfile_RK4, data["omega"], t);
+        t += 0.01;
+    }
+    auto end_RK = sc.now();
+
+
+    /*MyState<double> X{std::array<double,2>{1.00, 2.00}};
     MyState<double> Y{std::array<double,2>{3.00, 4.00}};
     X.print(X + Y);
     X.print(X / 2);
     X.print(X * 2);
     X = Y;
-    X.print(X);
+    X.print(X);*/
 
+    auto time_span_e = static_cast<std::chrono::duration<double>>(end_eu - start_eu);
+    auto time_span_h = static_cast<std::chrono::duration<double>>(end_h - start_h);
+    auto time_span_RK = static_cast<std::chrono::duration<double>>(end_RK - start_RK);
+
+    std::cout << time_span_e.count() << " " << time_span_h.count() << " " << time_span_RK.count();
+
+    myfile_e.close();
+    myfile_h.close();
+    myfile_RK4.close();
     return 0;
 }
